a270ab3827a1032e9aa798782050c15600aa8c9a
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-21 14:56:44 +0700

    dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,9 @@
+void main() {
+  // dynamic bisa menampung semua tipe data
+  dynamic random = "Eko Mareko";
+  print(random);
+  random = 27;
+  print(random);
+  random = true;
+  print(random);
+}
