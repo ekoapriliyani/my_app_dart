e942db06a2ca237b62cd70038ddbd73a553e737c
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-21 13:56:30 +0700

    variable

--- bin/variable.dart
+++ bin/variable.dart
@@ -3,4 +3,22 @@ void main() {
   print(firstName);
   print(firstName);
   print(firstName);
+
+  final name = "Eko Apriliyani";
+  final time = DateTime.now();
+
+  print(name);
+  print(time);
+
+  // const isinya tidk bisa diubah2
+  const name3 = "Mareko";
+
+  late var result = getVelue();
+  print('variable dipanggil');
+  print(result);
+}
+
+String getVelue() {
+  print('getValue Berjalan');
+  return ('Nama Saya Eko');
 }
