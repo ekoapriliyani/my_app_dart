dd923518ebf49c7894546cf0ec63dc83ea08c929
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-21 14:10:35 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 27;
+  double pi = 3.14;
+
+  // num : nilainya bisa integer maupun double
+  num nomorRumah = 23;
+
+  print(age);
+  print(pi);
+  print(nomorRumah);
+}
