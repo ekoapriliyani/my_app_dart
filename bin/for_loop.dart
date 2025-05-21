e1cbccd701c842855b57baa91ec46e6b0cd8e959
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-21 19:27:38 +0700

    for loop

---
+++ bin/for_loop.dart
@@ -0,0 +1,12 @@
+void main() {
+  // for loop
+  for (var i = 1; i <= 10; i++) {
+    print("Ini adalah angka $i");
+  }
+
+  // for in
+  List<String> names = ["Eko", "Nisa", "April", "Atun"];
+  for (var value in names) {
+    print(value);
+  }
+}
