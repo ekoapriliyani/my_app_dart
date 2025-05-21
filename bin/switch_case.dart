fe0cd5dbc373317e19bd7ef30aa63f65f0457e97
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-21 19:18:51 +0700

    switch case

---
+++ bin/switch_case.dart
@@ -0,0 +1,17 @@
+void main() {
+  var num = 1;
+
+  switch (num) {
+    case 1:
+      print("satu");
+      break;
+    case 2:
+      print("dua");
+      break;
+    case 3:
+      print("tiga");
+      break;
+    default:
+      print("tidak dikenali");
+  }
+}
