7ac4f8f5fb3f17a1ef3fdba8639f72f8349543bc
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-22 22:06:25 +0700

    recursive & stack overflow

--- bin/recursive_function.dart
+++ bin/recursive_function.dart
@@ -1,4 +1,6 @@
 // menggunakan for loop
+import 'dart:math';
+
 int factorialLoop(int value) {
   var result = 1;
   for (var i = 1; i <= value; i++) {
@@ -16,8 +18,23 @@ int factorialRecursive(int value) {
   }
 }
 
+// hati hati dalam menggunakan recursive, karena bisa stack overflow
+// contoh
+
+void loop(int value) {
+  if (value == 0) {
+    print("Selesai");
+  } else {
+    print("Perulangan ke-$value");
+    loop(value - 1);
+  }
+}
+
 void main() {
   print(factorialLoop(10));
   print(10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1);
   print(factorialRecursive(10));
+
+  loop(1000); // masih OK
+  loop(100000000000); // stack overflow
 }
